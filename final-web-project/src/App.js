aed9c1c078047d7ac4b79a954627aa65b4803384
fernanortega-hub <00191820@uca.edu.sv>
2021-11-13 15:45:36 -0600

    Implementing react-router-dom

--- final-web-project/src/App.js
+++ final-web-project/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import User from './Components/Login';
+import Login from './Pages/Login';
+import User from './Pages/User'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex justify-center items-center bg-gray-300 h-screen">
-      <User/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login"
+          element={
+            <div className="flex justify-center items-center bg-gray-300 h-screen">
+              <Login />
+            </div>}
+        />
+        <Route path="/principal"
+          element={
+            <div className="flex justify-center items-center bg-gray-300 h-screen">
+              <User />
+            </div>}
+        />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ final-web-project/src/Pages/User.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const User = () => { 
+    return (
+        <h1 className = "text-3xl font-bold bg-red-400"> Hola </h1>
+    );
+};
+
+export default User;
