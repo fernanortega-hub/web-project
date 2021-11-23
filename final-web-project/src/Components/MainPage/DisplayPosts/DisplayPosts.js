1a92b723a80eb312b275e16da6df839771f484c5
00190220 <00190220@uca.edu.sv>
2021-11-22 18:08:40 -0600

    Get post from api, Loading page

--- final-web-project/src/Components/MainPage/DisplayPosts/DisplayPosts.js
+++ final-web-project/src/Components/MainPage/DisplayPosts/DisplayPosts.js
@@ -1,6 +1,7 @@
 import PostCard from '../DisplayPosts/PostCard/PostCard';
 import Loading from '../Loading/Loading';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DisplayPosts = () =>{
 
@@ -10,15 +11,27 @@ const DisplayPosts = () =>{
     });
 
 
+    useEffect( ()=>{
+        const getPost = async () =>{
+                const {data: response} = await axios.get('', {
+                    headers: {
+                        Authorization: `Bearear ${localStorage.getItem('token')}`,
+                        },
+                });  
+            setPost({status: 'DONE', data: response.data});
+        }
+        getPost();
+
+    }, []);
+
+    if (Post.status === 'Loading') return <Loading/>
+        
     return(
         <div className="w-4/5 min-h-screen bg-gray-100 border rounded-lg shadow-lg my-4 tablet:w-2/3">
             {/* <Loading/> */}
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
-            <PostCard/>
+            {
+                Post.data && Post.data.map((it) => <PostCard key= {it._id} struct={it}/>)
+            }
             
         </div>
     );
--- final-web-project/src/Components/MainPage/DisplayPosts/PostCard/PostCard.js
+++ final-web-project/src/Components/MainPage/DisplayPosts/PostCard/PostCard.js
@@ -1,18 +1,38 @@
+import react from "react";
 import { FaUserCircle } from "react-icons/fa";
-const PostCard = () => {
+import {BiLike, BiMessageRounded} from "react-icons/bi";
+
+
+const PostCard = ({struct}) => {
+
+    /*const {
+        title, description, image, user, likes, createdAt, comments
+    } = struct;*/
+
+
 
     return (
-        <div className=" m-4 mx-10 rounded bg-gray-300 p-4 shadow-inner space-y-2">
-            <div className="flex flex-nowrap items-center space-x-3" >
+        <div className="flex flex-col m-8  rounded bg-gray-300 p-4 shadow-inner space-y-12 lg:w-10/12 lg:ml-28 lg: ">
+            <div className="flex flex-nowrap items-center space-x-3" > {/*Mostrar nombre de usuario*/}
                 <FaUserCircle size="25"/>
-                <h3> @Usuario </h3>
+                <h3> @{/*{user?.username}*/}User </h3>
             </div>
             
-            <div className="border border-black rounded h-44 text-center">
-                imagen
-            </div>
-            <p className="font-semibold">"Titulo: titulo de post"</p>
+            {          
+                <img className="w-full h-40 object-cover my-2 rounded-2xl" src={{/*image*/}} alt="Imagen para el usuario" />
+            }        
+            <p className="font-semibold">"Titulo: titulo de post"{/*title */} </p>
             <p>"Descripcion: descripcion de post"</p>
+            <div className="flex justify-center space-x-8">
+                <div className="flex justify-center">
+                    <button><span><BiLike className="mr-2" size={25}/></span></button>
+                    {/*likes.length*/}1
+                </div>
+                <div className="flex justify-center">
+                    <button><span><BiMessageRounded className="mr-2 " size={25}/></span> </button>
+                    {/*comments.length*/}2
+                </div>
+            </div>
         </div>
     );
 }
