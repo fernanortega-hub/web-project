3d2f921c03ac88f96c259f9eea2a15e8b5a9a607
Diego <00144019@uca.edu.sv>
2021-11-27 23:39:07 -0600

    Added and initialized profile page

--- final-web-project/src/App.js
+++ final-web-project/src/App.js
@@ -3,6 +3,8 @@ import Login from './Pages/Login';
 import MainPage from './Pages/MainPage';
 import NotFound from './Pages/NotFound';
 import Favorites from './Pages/Favorites';
+import Profile from './Pages/Profile';
+
 
 import {
   BrowserRouter as Router,
@@ -33,6 +35,22 @@ const App = () => {
           element={
             <div>
               <Favorites />
+            </div>
+          }
+        />
+
+        <Route exact path="/favorites"
+          element={
+            <div>
+              <Favorites />
+            </div>
+          }
+        />
+
+        <Route exact path="/profile"
+          element={
+            <div>
+              <Profile />
             </div>
           }
         />
