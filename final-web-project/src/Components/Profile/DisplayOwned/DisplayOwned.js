9dd9129a307475aad197867d51209f030e50b9df
Diego <00144019@uca.edu.sv>
2021-11-28 00:14:14 -0600

    Added DisplayOwned component && implemented getOwned api request

---
+++ final-web-project/src/Components/Profile/DisplayOwned/DisplayOwned.js
@@ -0,0 +1,67 @@
+import Loading from '../../Loadings/Loading';
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import PostCard from '../../MainPage/DisplayPosts/PostCard/PostCard';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+
+
+
+const DisplayOwned = ({username}) => {
+    
+    const [Post, setPost] = useState({
+        status: 'Loading',
+        data: null,
+    });
+
+    const pages = useRef();
+    const [page, setpage] = useState(0);
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+        };
+        const getOwned = async () => {
+
+            const { data: response } = await axios.get(`https://posts-pw2021.herokuapp.com/api/v1/post/owned?limit=15&page=${page}`, config);
+            setPost({ status: 'Ok', data: response.data });
+            pages.current = response.pages;
+        };
+        getOwned();
+    }, [page]); 
+
+    if (Post.status === 'Loading') return <Loading />;
+    
+    return(
+        <div className="w-full min-h-screen bg-gray-100 rounded-xl space-y-10 flex flex-col py-8 items-center laptop:w-1/2 dark:bg-gray-600">
+        {
+            Post.data && Post.data.map((it) => <PostCard key={it._id} struct={it} username={username} />)
+        }
+
+        <div className="flex items-center h-14 justify-center dark:bg-gray-600">
+            <button className="bg-gray-400 m-2 p-2 rounded-lg text-white flex items-center hover:bg-gray-600"
+                onClick={() => {
+                    setpage(page - 1);
+                    if (page === 0) setpage(page);
+                }}>
+                <FaArrowLeft className="mr-1" />
+            </button>
+            <span className="bg-gray-400 my-3 px-2 py-1 rounded-lg text-white font-medium" > {page + 1} </span>
+            <button className="bg-gray-400 m-2 p-2 rounded-lg text-white flex items-center hover:bg-gray-600"
+                onClick={() => {
+                    setpage(page + 1);
+                    if (page >= pages) {
+                        toast('No hay mas paginas', { type: 'Warning' });
+                        setpage(page - 1);
+                    }
+
+                }}>
+                <FaArrowRight className="ml-1" />
+            </button>
+        </div>
+    </div>
+    );
+}
+export default DisplayOwned;
--- final-web-project/src/Pages/Profile.js
+++ final-web-project/src/Pages/Profile.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import DisplayOwned from "../Components/Profile/DisplayOwned/DisplayOwned";
+import { Auth } from "../Services/Services";
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
 const Profile = () => {
+    const [username, setUser] = useState();
+    const token = localStorage.getItem('token')
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            try {
+                const response = await Auth(token);
+                setUser(response.username);
+            } catch (error) {
+                toast('Algo salio mal, inicia sesion nuevamente', { type: 'error' });
+                navigate('/');
+            }
+        }
+        verifyUser();
+        document.title = "Principal";
+    }, []);
+
 
     return(
-        <h1>Profile</h1>
+        <div className="flex flex-col justify-items-center items-center bg-gray-200 gap-3 overflow-x-hidden dark:bg-gray-900">
+            <DisplayOwned username={username} />
+        </div>
     );
 }
 
